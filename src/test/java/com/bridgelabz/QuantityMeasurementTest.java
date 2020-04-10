d23c0368d5f51b50f37a12f58a8497a161b2280f
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-10 17:49:25 +0530

    ADDED:Test Case To Check Feet To Yard Conversion

--- src/test/java/com/bridgelabz/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -78,5 +79,23 @@ public class QuantityMeasurementTest {
         double value1 = quantityMeasurement.returnInches(2.2);
         double value2 = quantityMeasurement.returnInches(2.2);
         assertEquals(value1, value2, 0.0);
+    }
+
+    @Test
+    public void givenValueInFeet_ShouldReturnValueInInches() {
+        double inchValue = quantityMeasurement.returnValueInInches(1);
+        Assert.assertEquals(12, inchValue, 0.0);
+    }
+
+    @Test
+    public void givenValueInInch_ShouldReturnValeInFeet() {
+        double feetValue = quantityMeasurement.returnValueInFeet(24);
+        Assert.assertEquals(2, feetValue, 0.0);
+    }
+
+    @Test
+    public void givenValueInFeet_ShouldReturnValueInYard() {
+        double yardValue = quantityMeasurement.returnValueInYard(3);
+        Assert.assertEquals(1, yardValue, 0.0);
     }
 }
