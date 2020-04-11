387eeb36bb613f6c79a40bcf1d111268c5303c5e
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-11 15:31:53 +0530

    ADDED:Enum Constant For Temperature

--- src/main/java/com/bridgelabz/Unit.java
+++ src/main/java/com/bridgelabz/Unit.java
@@ -4,7 +4,8 @@ public enum Unit {
     //ENUM CONSTANTS
     FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4),
     GALLON(3.78), LITRE(1), MILLILITRE(0.001),
-    GRAM(0.001), TONNE(1000);
+    GRAM(0.001), TONNE(1000),
+    CELSIUS(1.8), FAHRENHEIT(1);
 
     //FIELD
     public double type;
