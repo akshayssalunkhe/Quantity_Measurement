52e57a8594a9a65463ae8987d0e3fa4ea7e279f2
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-11 12:58:21 +0530

    ADDED:Enum Constant For Weights

--- src/main/java/com/bridgelabz/Unit.java
+++ src/main/java/com/bridgelabz/Unit.java
@@ -3,7 +3,8 @@ package com.bridgelabz;
 public enum Unit {
     //ENUM CONSTANTS
     FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4),
-    GALLON(3.78), LITRE(1), MILLILITRE(0.001);
+    GALLON(3.78), LITRE(1), MILLILITRE(0.001),
+    GRAM(0.001), TONNE(1000);
 
     //FIELD
     public double type;
