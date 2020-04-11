ce562c53276e209d57edb115fe93a1655438a1c5
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-11 11:21:37 +0530

    ADDED:Enum Constant For Centimeter

--- src/main/java/com/bridgelabz/Unit.java
+++ src/main/java/com/bridgelabz/Unit.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public enum Unit {
     //ENUM CONSTANTS
-    FEET(12.0), INCH(1.0), YARD(36.0);
+    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4);
 
     //FIELD
     public double type;
