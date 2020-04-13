0f757d0c5b8df47326591fa8cad4d8da3c10d23c
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-13 23:26:22 +0530

    ADDED:Enum Constant For Kilogram And Field For Quantity Type

--- src/main/java/com/bridgelabz/Unit.java
+++ src/main/java/com/bridgelabz/Unit.java
@@ -2,16 +2,19 @@ package com.bridgelabz;
 
 public enum Unit {
     //ENUM CONSTANTS
-    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4),
-    GALLON(3.78), LITRE(1), MILLILITRE(0.001),
-    GRAM(0.001), TONNE(1000),
-    CELSIUS(1.8), FAHRENHEIT(1);
+    FEET(12.0, Type.LENGTH), INCH(1.0, Type.LENGTH), YARD(36.0, Type.LENGTH), CENTIMETER(0.4, Type.LENGTH),
+    GALLON(3.78, Type.VOLUME), LITRE(1, Type.VOLUME), MILLILITRE(0.001, Type.VOLUME),
+    GRAM(0.001, Type.WEIGHT), TONNE(1000, Type.WEIGHT), KILOGRAM(1, Type.WEIGHT),
+    CELSIUS(1.8, Type.TEMPERATURE), FAHRENHEIT(1, Type.TEMPERATURE);
+
 
     //FIELD
     public double type;
+    Type quantityType;
 
     //CONSTRUCTOR
-    Unit(double type) {
+    Unit(double type, Type quantityType) {
         this.type = type;
+        this.quantityType = quantityType;
     }
 }
